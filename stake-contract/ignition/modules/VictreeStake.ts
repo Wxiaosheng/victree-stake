85f1ffb1f21162f5784bdc1c95bcb0fcd61c4385
victree <tian__zun@163.com>
2025-09-18 13:49:27 +0800

    feat: stake 页面对接 合约

--- stake-contract/ignition/modules/VictreeStake.ts
+++ stake-contract/ignition/modules/VictreeStake.ts
@@ -1,5 +1,6 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
+// VictreeStakeModule#VictreeStake - 0xa9eC99e4e4566B029e8770d441AF2f2246581751
 export default buildModule("VictreeStakeModule", (m) => {
   // 创建 VictreeStake 合约实例
   const vs = m.contract("VictreeStake");
