f91d6baa3cf25b6c4de77d0235d55ea2a4d84472
victree <tian__zun@163.com>
2025-09-18 14:07:54 +0800

    feat: 获取相关金额，并解除质押

--- stake-fe/app/withdraw/page.tsx
+++ stake-fe/app/withdraw/page.tsx
@@ -1,26 +1,85 @@
 'use client';
 
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Button, Col, InputNumber, Row, Statistic } from "antd";
-import { parseEther } from "viem";
-import { useAccount } from "wagmi";
+import { formatEther, parseEther } from "viem";
+import { useAccount, useReadContracts, useWatchContractEvent, useWriteContract } from "wagmi";
+import abi from '../../abi/victreeStake.json';
 
 export default function Withdraw() {
   const { address, isConnected } = useAccount();
   const [unstakeAmount, setUnstakeAmount] = useState<number | null>();
   const [withdrawAmount, setWithdrawAmount] = useState<number | null>();
 
+  // 当前用户质押金额
+  const { data } = useReadContracts({
+    contracts: [
+      { // 质押金额
+        abi, // 替换为你的合约 ABI
+        address: '0xa9eC99e4e4566B029e8770d441AF2f2246581751',
+        functionName: 'getUserTotalStaked',
+        args: [address],
+      },
+      { // 锁定金额
+        abi,
+        address: '0xa9eC99e4e4566B029e8770d441AF2f2246581751',
+        functionName: 'getCooldownAmount',
+        args: [address],
+      },
+      { // 可提取金额
+        abi,
+        address: '0xa9eC99e4e4566B029e8770d441AF2f2246581751',
+        functionName: 'getWithdrawableAmount',
+        args: [address],
+      }
+    ]
+  });
+
+  // 质押金额、锁定金额、可提取金额
+  const [staked, cooldown, withdraw] = useMemo(() => {
+    const [staked, cooldown, withdraw] = data || [];
+
+    return [
+      staked ? formatEther(staked.result as bigint) : 0,
+      cooldown ? formatEther(cooldown.result as bigint) : 0,
+      withdraw ? formatEther(withdraw.result as bigint) : 0,
+    ];
+  }, [data]);
+
+  // 监听 StakeChanged 事件，实时更新质押金额
+  useWatchContractEvent({
+    address: '0xa9eC99e4e4566B029e8770d441AF2f2246581751',
+    abi,
+    eventName: 'UnStaked',
+    onLogs(logs) {
+      console.log('UnStaked event:', logs);
+      alert('UnStaked event: ' + JSON.stringify(logs));
+    }
+  })
+  
+  const { writeContract } = useWriteContract();
+
+  /**
+   * 解除质押
+   */
   const handleUnstake = () => {
-    console.log("User address:", address);
-    console.log("Is connected:", isConnected);
     if (!isConnected) {
       throw new Error("Please connect your wallet first");
     }
 
     const amount = parseEther(`${unstakeAmount || 0}`);
-    console.log("Unstake clicked", amount);
+    
+    writeContract({
+      abi,
+      address: '0xa9eC99e4e4566B029e8770d441AF2f2246581751', // 替换为你的合约地址
+      functionName: 'unStakeETH',
+      args: [amount],
+    });
   }
   
+  /**
+   * 提取质押金额
+   */
   const handleWithdraw = () => {
     console.log("User address:", address);
     console.log("Is connected:", isConnected);
@@ -36,8 +95,7 @@ export default function Withdraw() {
     <Row gutter={[16, 16]} style={{ marginTop: 30 }} >
       <Col span={8} className="text-center">
         <Statistic 
-          value={112893} 
-          precision={2} 
+          value={staked} 
           title={<>
             <p className="text-blue-500 text-2xl">质押金额(ETH)</p>
             <p className="text-sm text-gray-500 mt-1">质押金额是指您当前质押在合约中的总金额。</p>
@@ -46,8 +104,7 @@ export default function Withdraw() {
       </Col>
       <Col span={8} className="text-center">
         <Statistic
-          value={112893} 
-          precision={2} 
+          value={cooldown} 
           title={<>
             <p className="text-blue-500 text-2xl font-weight-bold">锁定金额(ETH)</p>
             <p className="text-sm text-gray-500 mt-1">锁定金额是指您解除质押后存在 20 min 的冷静期，冷静期内您无法提取该金额。</p>
@@ -56,8 +113,7 @@ export default function Withdraw() {
       </Col>
       <Col span={8} className="text-center">
         <Statistic 
-          value={112893} 
-          precision={2} 
+          value={withdraw} 
           title={<>
             <p className="text-blue-500 text-2xl font-weight-bold">可提取金额(ETH)</p>
             <p className="text-sm text-gray-500 mt-1">可提取金额是指解除质押后，超过冷静期的，可以立即提取的金额。</p>
